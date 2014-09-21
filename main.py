c59583c3b2ebc8dd4449e2fad3ec627905b24e3f
Vikash Kumar <mailkumarvikash@gmail.com>
2014-09-21 16:05:24 +0530

    biggest commit yet

--- main.py
+++ main.py
@@ -1,25 +1,102 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import jinja2
+import urllib2
+import os.path
+import json
+
+from urllib2 import Request, urlopen
+from google.appengine.ext.webapp import template
+from google.appengine.ext import ndb
+from jinja2 import Template
+
+
+
+class Main(ndb.Model):
+  age = ndb.StringProperty()
+  sex = ndb.StringProperty()
+  part = ndb.StringProperty()
+  sympt = ndb.StringProperty()
+  docter = ndb.StringProperty()
+
+
+
+
+
+headers = {
+  'X-API-KEY': 'RIXjc9pnVHFNfwL/KJzohowzr10=',
+  'X-CLIENT-ID': '90797d50-03ea-4825-8441-e561b74606fe'
+}
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    autoescape=True,
+    extensions=['jinja2.ext.autoescape'])
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
+
+    def post(self):
+        prt = self.request.get('part')
+        smpt = self.request.get('sympt')
+        special = Main.query(Main.part==prt, Main.sympt==smpt).get()
+
+        url = 'https://api.practo.com/search/?city=bangalore&locality=whitefield&speciality={0}&filters=filters[qualification]'.format(special.docter)
+
+        request = Request(url, headers=headers)
+        response_body = urlopen(request).read()
+        url = json.loads(response_body)
+        params = {
+            'v' : url
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('result.html')
+        self.response.write(template.render(params))
+
+
+
+class DocterProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        self.render_template('form.html')
+
+    def post(self):
+        part = self.request.get('part')
+        sympt = self.request.get('sympt')
+        doct = self.request.get('doct')
+
+        data = Main(age="18-20",
+                    sex="male",
+                    part=part,
+                    sympt=sympt,
+                    docter=doct)
+        data.put()
+        self.render_template('form.html')
+
+    def render_template(self, view_filename, params=None):
+        if not params:
+            params = {}
+        path = os.path.join(os.path.dirname(__file__), 'views', view_filename)
+        self.response.out.write(template.render(path, params))
+
+
+class ConfermHandler(webapp2.RequestHandler):
+    def post(self,**kwargs):
+        name = kwargs['name']
+        params = {
+            'v' : name
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('endpage.html')
+        self.response.write(template.render(params))
+
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    webapp2.Route('/', handler=MainHandler),
+    webapp2.Route('/profile', handler=DocterProfileHandler),
+    webapp2.Route('/conferm/<name:.*>', handler=ConfermHandler)
 ], debug=True)
